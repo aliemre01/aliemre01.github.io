d8b3eadc3ba4371e03cc52167d3a69c7946968f7
aliemre01 <aliemreboga3@gmail.com>
2025-03-22 23:59:45 +0300

    Update Hw1-Q4a.c
    Add HW1 Question 4 solution

--- Hw1-Q4a.c
+++ Hw1-Q4a.c
@@ -1,7 +1,9 @@
+// For Question 4-A 
+
 #include <stdio.h>
 #include <math.h>
 
-// Ev değerini hesaplayan fonksiyon
+
 int computeHomeValue(int popularity, int size)
 {
     return (pow(popularity, 3) + pow(size, 2)) * 10000;
@@ -11,14 +13,14 @@ int main(void)
 {
     int popularity, size;
 
-    // Kullanıcıdan verileri al
+   
     printf("Enter popularity: ");
     scanf("%d", &popularity);
 
     printf("Enter size: ");
     scanf("%d", &size);
 
-    // Fonksiyonu çağırarak ev değerini hesapla ve yazdır
+   
     int value = computeHomeValue(popularity, size);
     printf("Home value is: %d\n", value);
 
