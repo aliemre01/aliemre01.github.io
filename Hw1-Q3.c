dc6a1179914cb40d88e2286c43d925d920c2ffea
aliemre01 <aliemreboga3@gmail.com>
2025-03-22 23:57:44 +0300

    Update Hw1-Q3.c
    Add HW1 Question 3 solution

--- Hw1-Q3.c
+++ Hw1-Q3.c
@@ -6,17 +6,17 @@ int main(void)
     int popularity, size;
     int homeValue;
 
-    // Kullanıcıdan popularite ve boyut değerlerini al
+ 
     printf("Enter popularity: ");
     scanf("%d", &popularity);
 
     printf("Enter size: ");
     scanf("%d", &size);
 
-    // Formüle göre ev değerini hesapla: (popularity^3 + size^2) * 10000
+   
     homeValue = (pow(popularity, 3) + pow(size, 2)) * 10000;
 
-    // Sonucu ekrana yazdır
+    
     printf("Home value is: %d\n", homeValue);
     return 0;
 }
