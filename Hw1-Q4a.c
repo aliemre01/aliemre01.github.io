bc8cdd18c4d35aca26242508fd670d9e07f5f5fd
aliemre01 <aliemreboga3@gmail.com>
2025-03-23 00:13:26 +0300

    Update Hw1-Q4a.c

--- Hw1-Q4a.c
+++ Hw1-Q4a.c
@@ -28,32 +28,3 @@ int main(void)
 }
 
 
-// For Question 4-B
-
-#include <stdio.h>
-#include <math.h>
-
-
-void computeHomeValue(void)
-{
-    int popularity, size;
-    int homeValue;
-
-    printf("Enter popularity: ");
-    scanf("%d", &popularity);
-
-    printf("Enter size: ");
-    scanf("%d", &size);
- 
-  
-    homeValue = (pow(popularity, 3) + pow(size, 2)) * 10000;
-
-    printf("Home value is: %d\n", homeValue);
-}
-
-int main(void)
-{
-
-    computeHomeValue();
-    return 0;
-}
